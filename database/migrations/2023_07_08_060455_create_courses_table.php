16dd2c011171c27118c1ad83b8fe387505b731aa
Diaz  Luthfi <diazluthfi07@gmail.com>
2023-07-09 14:24:16 +0700

    udh bil

--- database/migrations/2023_07_08_060455_create_courses_table.php
+++ database/migrations/2023_07_08_060455_create_courses_table.php
@@ -13,17 +13,18 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('address');
-            $table->string('phone');
-            $table->string('role');
-            $table->rememberToken();
+            $table->integer('price');
+            $table->string('image');
+            $table->unsignedBigInteger('field_id');
             $table->timestamps();
+
+            $table->foreign('field_id')->references('id')->on('fields');
+
+
+    
         });
     }
 
@@ -34,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('courses');
     }
 };
