a00bd33cede4994aab49c0122b6a44b754397f89
Diaz  Luthfi <diazluthfi07@gmail.com>
2023-08-01 21:11:23 +0700

    add admin

---
+++ resources/views/dashboard/courses.blade.php
@@ -0,0 +1,53 @@
+@extends('dashboard.layouts.master')
+
+@section('content')
+
+
+<div class="row">
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col" style="padding-left: 30px">No</th>
+          <th scope="col">Nama</th>
+          <th scope="col">Price</th>
+          <th scope="col">Image</th>
+          <th scope="col">Fiel Id</th>
+          <th scope="col">Dibuat</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($courses as $row)
+        <tr>
+          <th scope="row" style="padding-left: 30px">{{$row->id}}</th>
+          <td>{{$row->name}}</td>
+          <td>{{$row->price}}</td>
+          <td>
+            <img style="width: 50px" src="{{ asset('storage/images/' . $row->image) }}" alt="">
+          </td>
+         
+          <td>{{$row->field_id}}</td>
+          <td>{{$row->created_at->diffForHumans()}}</td>
+          {{-- <td><a href="{{ route ('', $row->id)}}" type="button" class="btn btn-success">Update</a> --}}
+                
+        
+              <td>  
+           
+            <button type="button" class="btn btn-danger" onclick="document.getElementById('form-{{$row->id}}').submit()">Hapus</button> 
+            <form id="form-{{$row->id}}" action="{{ route('delete.courses', $row->id) }}" method="POST">
+              @csrf
+              @method('delete')
+            </form>
+
+            
+         
+           </td>
+        </tr>
+        @endforeach
+        
+       
+      </tbody>
+    </table>
+  </div>
+    @endsection
---
+++ resources/views/dashboard/layouts/master.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Blank Page</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('admin-theme') }}/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('admin-theme') }}/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+
+      {{-- <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li> --}}
+      {{-- <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li> --}}
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="{{ asset('admin-theme') }}/index3.html" class="brand-link">
+      <img src="{{ asset('admin-theme') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">TOURISM</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="{{ asset('admin-theme') }}/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+        </div>
+      </div>
+
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-header">Dashboard</li>
+
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="nav-icon fas fa-ellipsis-h"></i>
+              <p>User</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{route('dashboard.courses')}}" class="nav-link">
+              <i class="nav-icon fas fa-ellipsis-h"></i>
+              <p>Courses</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="nav-icon fas fa-ellipsis-h"></i>
+              <p>Orders</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link">
+              <i class="nav-icon fas fa-ellipsis-h"></i>
+              <p>Setifikat</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" onclick="document.getElementById('logout-form').submit()" class="nav-link">
+              <i class="nav-icon fas fa-sign-out-alt"></i>
+              <p>Sign Out</p>
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST">
+              @csrf
+            </form>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    @yield('content')
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{ asset('admin-theme') }}/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('admin-theme') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('admin-theme') }}/dist/js/adminlte.min.js"></script>
+</body>
+</html>
