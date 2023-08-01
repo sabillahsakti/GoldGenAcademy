2d069af877b20ff063775c5950077f0c707b633f
bobyseptiananda12 <bobymunthe48@gmail.com>
2023-08-02 01:54:43 +0700

    add css for purchase

--- resources/views/account_info.blade.php
+++ resources/views/account_info.blade.php
@@ -14,87 +14,102 @@
     <script src='main.js'></script>
 
     <style>
-        .dropdown {
-            position: relative;
-            display: inline-block;
-        }
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
 
-        .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #f9f9f9;
-            min-width: 160px;
-            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-            z-index: 1;
-        }
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        z-index: 1;
+    }
 
-        .dropdown-content a {
-            color: black;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
-        }
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
 
-        .dropdown-content a:hover {
-            background-color: #f1f1f1;
-        }
+    .dropdown-content a:hover {
+        background-color: #f1f1f1;
+    }
 
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
+    .dropdown:hover .dropdown-content {
+        display: block;
+    }
 
-        .btn-change-password {
-            display: inline-block;
-            padding: 10px 20px;
-            margin-top: 10px;
-            color: white;
-            background-color: #0041E8;
-            border: none;
-            cursor: pointer;
-            transition: background-color 0.3s ease;
-            text-align: center;
-        }
+    .btn-change-password {
+        display: inline-block;
+        padding: 10px 20px;
+        margin-top: 10px;
+        color: white;
+        background-color: #0041E8;
+        border: none;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+        text-align: center;
+    }
 
-        .btn-change-password:hover {
-            background-color: #0031b8;
-        }
+    .btn-change-password:hover {
+        background-color: #0031b8;
+    }
 
+    .button-cp button {
+        display: block;
+        padding: 15px;
+        background-color: #333;
+        color: #fff;
+        border: none;
+        border-radius: 8px;
+        cursor: pointer;
+        margin-left: 573px;
+    }
+
+    .button-cp button:hover {
+        background-color: #666;
+        /* Darker shade on hover */
+    }
     </style>
 
     <script>
-        window.onscroll = function() {
-            scrollFunction();
-        };
+    window.onscroll = function() {
+        scrollFunction();
+    };
 
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                document.getElementById("scrollBtn").style.display = "block";
-            } else {
-                document.getElementById("scrollBtn").style.display = "none";
-            }
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("scrollBtn").style.display = "block";
+        } else {
+            document.getElementById("scrollBtn").style.display = "none";
         }
+    }
 
-        function scrollToTop() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        }
+    function scrollToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
     </script>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
-        $(document).ready(function() {
-            // Hide the dropdown when clicking outside of it
-            $(document).on('click', function(event) {
-                if (!$(event.target).closest('.dropdown').length) {
-                    $('.dropdown-content').hide();
-                }
-            });
-
-            // Show/hide the dropdown when clicking on the username button
-            $('.dropdown-btn').on('click', function() {
-                $('.dropdown-content').toggle();
-            });
+    $(document).ready(function() {
+        // Hide the dropdown when clicking outside of it
+        $(document).on('click', function(event) {
+            if (!$(event.target).closest('.dropdown').length) {
+                $('.dropdown-content').hide();
+            }
         });
+
+        // Show/hide the dropdown when clicking on the username button
+        $('.dropdown-btn').on('click', function() {
+            $('.dropdown-content').toggle();
+        });
+    });
     </script>
 </head>
 
@@ -117,18 +132,18 @@
 
             <div class="ltalk-container">
                 @if(session('user'))
-                    <div class="dropdown">
-                        <button class="ltalk dropdown-btn">{{ session('user')->name }}</button>
-                        <div class="dropdown-content">
-                            <a href="{{ url('account') }}">Account Information</a>
-                            <a href="{{ route('myCourses') }}">My Courses</a>
-                            <a href="{{ route('logout') }}">Logout</a>
-                        </div>
+                <div class="dropdown">
+                    <button class="ltalk dropdown-btn">{{ session('user')->name }}</button>
+                    <div class="dropdown-content">
+                        <a href="{{ url('account') }}">Account Information</a>
+                        <a href="{{ route('myCourses') }}">My Courses</a>
+                        <a href="{{ route('logout') }}">Logout</a>
                     </div>
+                </div>
                 @else
-                    <button class="ltalk" onclick="window.location.href ='{{url('login')}}';">
-                        Login
-                    </button>
+                <button class="ltalk" onclick="window.location.href ='{{url('login')}}';">
+                    Login
+                </button>
                 @endif
             </div>
 
@@ -142,16 +157,17 @@
                     <i class="fas fa-user-circle fa-4x" id="userimg"></i>
                     <p class="h4-bagiankiri">{{ $user->name }}</p>
                     <p class="h4-bagiankiri" style="color: gray">{{ $user->email }}</p>
-                    
+
                 </div>
             </div>
             <div class="bagian-kanan">
                 <p class="p-info">Informasi Akun</p>
-                <p  style="color: gray" class="p2-info">Manage your personal information, including phonenumber and email address where you can be contatcted</p>
+                <p style="color: gray" class="p2-info">Manage your personal information, including phonenumber and email
+                    address where you can be contatcted</p>
                 <div class="isian">
                     <div class="kotakan">
                         <div class="dalemanan">
-                                <p class="atas-header">Name</p>
+                            <p class="atas-header">Name</p>
                             <p class="atas-p">{{ $user->name }}</p>
                         </div>
                     </div>
@@ -187,18 +203,20 @@
                         </div>
                     </div>
                 </div>
-                <button onclick="window.location.href='{{route('changePasswordForm')}}';" class="btn-change-password">
-                    Change Password
-                </button>
-
+                <div class="button-cp">
+                    <button onclick="window.location.href='{{route('changePasswordForm')}}';"
+                        class="btn-change-password">
+                        Change Password
+                    </button>
+                </div>
             </div>
         </div>
     </div>
-    
+
     </div>
-    
+
     </div>
-    
+
 </body>
 
 </html>
--- resources/views/changePassword.blade.php
+++ resources/views/changePassword.blade.php
@@ -1,106 +1,115 @@
 @extends('layout.master')
 <style>
-    h2 {
-        text-align: center;
-        margin-bottom: 20px;
-    }
+h2 {
+    text-align: center;
+    margin-bottom: 20px;
+}
 
-    form {
-        width: 50%;
-        margin: 0 auto;
-        padding: 20px;
-        border: 1px solid #ddd;
-        border-radius: 5px;
-        background: #f9f9f9;
-    }
+form {
+    width: 50%;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    background: #f9f9f9;
+}
 
-    div {
-        margin-bottom: 10px;
-        padding: 5px;
-    }
+div {
+    margin-bottom: 10px;
+    padding: 5px;
+}
 
-    label {
-        display: block;
-        margin-bottom: 5px;
-    }
+label {
+    display: block;
+    margin-bottom: 5px;
+}
 
-    input[type="password"] {
-        width: 100%;
-        padding: 10px;
-        border: 1px solid #ddd;
-        border-radius: 5px;
-    }
+input[type="password"] {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+}
 
-    .alert {
-        color: #721c24;
-        background-color: #f8d7da;
-        border-color: #f5c6cb;
-        padding: .75rem 1.25rem;
-        margin-bottom: 1rem;
-        border: 1px solid transparent;
-        border-radius: .25rem;
-    }
+.alert {
+    color: #721c24;
+    background-color: #f8d7da;
+    border-color: #f5c6cb;
+    padding: .75rem 1.25rem;
+    margin-bottom: 1rem;
+    border: 1px solid transparent;
+    border-radius: .25rem;
+}
 
-    .alert-danger {
-        color: #721c24;
-        background-color: #f8d7da;
-        border-color: #f5c6cb;
-    }
+.alert-danger {
+    color: #721c24;
+    background-color: #f8d7da;
+    border-color: #f5c6cb;
+}
 
-    .alert-success {
-        color: #155724;
-        background-color: #d4edda;
-        border-color: #c3e6cb;
-    }
+.alert-success {
+    color: #155724;
+    background-color: #d4edda;
+    border-color: #c3e6cb;
+}
 
-    button[type="submit"] {
-        padding: 10px 20px;
-        background-color: #0275d8;
-        color: white;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        transition: background-color 0.3s ease;
-    }
+.btn-sub {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+}
 
-    button[type="submit"]:hover {
-        background-color: #014c8c;
-    }
+.btn-sub button[type="submit"] {
+    display: block;
+    background-color: #333;
+    padding: 15px;
+    color: #fff;
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+    justify-content: center;
+}
+
+.btn-sub button[type="submit"]:hover {
+    background-color: #666;
+    /* Darker shade on hover */
+}
 </style>
 
 @section('content')
-    <h2>Change Password</h2>
+<h2>Change Password</h2>
 
-    @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
-    @endif
+@if (session('status'))
+<div class="alert alert-success">
+    {{ session('status') }}
+</div>
+@endif
 
-    <form method="POST" action="{{ route('changePassword') }}">
-        @csrf
+<form method="POST" action="{{ route('changePassword') }}">
+    @csrf
 
-        <div>
-            <label for="old_password">Old Password</label>
-            <input type="password" id="old_password" name="old_password" required>
-            @error('old_password')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
+    <div>
+        <label for="old_password">Old Password</label>
+        <input type="password" id="old_password" name="old_password" required>
+        @error('old_password')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    </div>
 
-        <div>
-            <label for="new_password">New Password</label>
-            <input type="password" id="new_password" name="new_password" required>
-            @error('new_password')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
+    <div>
+        <label for="new_password">New Password</label>
+        <input type="password" id="new_password" name="new_password" required>
+        @error('new_password')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    </div>
 
-        <div>
-            <label for="new_password_confirmation">Confirm New Password</label>
-            <input type="password" id="new_password_confirmation" name="new_password_confirmation" required>
-        </div>
-
-        <button type="submit">Change Password</button>
-    </form>
+    <div>
+        <label for="new_password_confirmation">Confirm New Password</label>
+        <input type="password" id="new_password_confirmation" name="new_password_confirmation" required>
+    </div>
+    <div class="btn-sub">
+    <button type="submit">Change Password</button>
+    </div>
+</form>
 @endsection
