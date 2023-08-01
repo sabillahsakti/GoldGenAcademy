fd29ac88d84550780c50f0c290d6762ceb9c1118
bobyseptiananda12 <bobymunthe48@gmail.com>
2023-08-01 22:21:01 +0700

    add image at course page

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -2,41 +2,41 @@
 
 @section('content')
 
-    <div class="services-section">
-        <h2>Course List</h2>
-        <p>
-            Choose the course that you want    
-        </p>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">No</th>
-      <th scope="col">Name</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</table>
+<div class="services-section">
+    <h2>Course List</h2>
+    <p>
+        Choose the course that you want
+    </p>
 
-    
-        
-        <div class="service-card">
-            @foreach($courses as $course)
-            <div class="service">
-                <img src="{{ asset($course->image) }}" alt="Course Image">
-                <h3>{{ $course->name }}</h3>
-                <h5>{{ $course->price }}</h5>
-                <button onclick="window.location.href = '{{ Auth::check() ? route('courses.purchase', $course->id) : route('index') }}'">Purchase</button>
+    <div class="service-card">
+        @foreach($courses as $course)
+        <div class="service">
+            <style>
+            .button-purchase button {
+                display: block;
+                padding: 10px;
+                background-color: #333;
+                color: #fff;
+                border: none;
+                border-radius: 8px;
+                cursor: pointer;
+                margin-left: 111px;
+            }
+
+            .button-purchase button:hover {
+                background-color: #666;
+                /* Darker shade on hover */
+            }
+            </style>
+            <img src="{{ asset($course->image) }}" alt="Course Image">
+            <h3>{{ $course->name }}</h3>
+            <h5>{{ $course->price }}</h5>
+            <div class="button-purchase">
+            <button
+                onclick="window.location.href = '{{ Auth::check() ? route('courses.purchase', $course->id) : route('index') }}'">Purchase</button>
             </div>
-            @endforeach
         </div>
+        @endforeach
+    </div>
 
     @endsection
